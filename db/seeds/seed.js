e415ad3a9c6467de58da911807dfd4f9c765dcb3
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-04-18 14:29:49 +0100

    seed topics data

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,10 +1,19 @@
-// const {  } = require('../data');
+const {
+  articlesData,
+  commentsData,
+  topicsData,
+  usersData
+} = require("../data");
 
 exports.seed = (knex, Promise) => {
   return knex.migrate
     .rollback()
     .then(() => knex.migrate.latest())
     .then(() => {
-      // insert data
-    });
+      console.log("inserting topics data");
+      return knex("topics")
+        .insert(topicsData)
+        .returning("*");
+    })
+    .then(topicsRows => {});
 };
