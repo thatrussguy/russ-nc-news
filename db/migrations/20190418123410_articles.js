323d4922ab769e6c4a69e9d2ab143dbf246f6678
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-04-18 15:21:36 +0100

    change body types to text

--- db/migrations/20190418123410_articles.js
+++ db/migrations/20190418123410_articles.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("articles", articlesTable => {
     articlesTable.increments("article_id").primary();
     articlesTable.string("title").notNullable();
-    articlesTable.string("body").notNullable();
+    articlesTable.text("body").notNullable();
     articlesTable.integer("votes").defaultTo(0);
     articlesTable
       .string("topic")
