fb8784a3431886dd7e31af480eecd48a431093c6
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-04-18 12:31:58 +0100

    topics migration

---
+++ db/migrations/20190418122818_topics.js
@@ -0,0 +1,9 @@
+exports.up = function(knex, Promise) {
+  console.log("creating topics table");
+  return knex.schema.createTable("topics", topicsTable => {
+    topicsTable.string("slug").primary();
+    topicsTable.string("description");
+  });
+};
+
+exports.down = function(knex, Promise) {};
