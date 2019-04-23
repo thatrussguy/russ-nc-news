5038aab959ecb85e65cb0c1016ea9a6fb5383dbf
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-04-23 08:16:24 +0100

    Reseed database before each test

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,21 +1,21 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
-const { expect } = require('chai');
-const supertest = require('supertest');
+const { expect } = require("chai");
+const supertest = require("supertest");
 
-const app = require('../app');
-const connection = require('../db/connection');
+const app = require("../app");
+const connection = require("../db/connection");
 
 const request = supertest(app);
 
-describe('/', () => {
-  // beforeEach(() => connection.seed.run());
+describe("/", () => {
+  beforeEach(() => connection.seed.run());
   after(() => connection.destroy());
 
-  describe('/api', () => {
-    it('GET status:200', () => {
+  describe("/api", () => {
+    it("GET status:200", () => {
       return request
-        .get('/api')
+        .get("/api")
         .expect(200)
         .then(({ body }) => {
           expect(body.ok).to.equal(true);
