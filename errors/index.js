775d68ab74a1efaa80978c18bc3fc96d5502362b
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-04-24 10:29:26 +0100

    error handling

--- errors/index.js
+++ errors/index.js
@@ -13,5 +13,5 @@ exports.handle500 = (err, req, res, next) => {
 
 exports.handle400 = (err, req, res, next) => {
   if (err.status === 400) res.status(err.status).send({ message: err.message });
-  else next();
+  else next(err);
 };
